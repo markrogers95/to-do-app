96861aa92aa030f582e7992f4dfb5b81bdd59f59
Mark Rogers <mark_199518@hotmail.co.uk>
2020-07-23 21:31:13 +0100

    First commit

---
+++ server/data-models/to-do.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const validationString = {
+    type: String,
+    require: true,
+};
+
+var toDoListSchema = new Schema({
+    title: validationString,
+    description: validationString,
+    dueDate: {
+        type: Date,
+        required: true,
+    },
+    critical: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 3,
+    }, {
+        timestamps: true
+    }
+);
+
+const toDoItem = mongoose.model('toDoItem', toDoListSchema);
+
+module.exports = toDoItem;
---
+++ server/src/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const env = require('dotenv').config();
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+if (env.error){
+    // throw env.error;
+    console.log(env.error);
+}
+
+const app = express();
+const port = process.env.PORT || 1337;
+
+mongoose.connect('mongodb://127.0.0.1/users', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).catch(error => handleError(error));
+
+app.use(morgan('common'));
+app.use(helmet());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        "message": "hello world !"
+    });
+});
+
+app.use((req, res, next) => {
+    const error = new Error(`Not found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? "No stack" : error.stack
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
